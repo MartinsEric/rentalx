c6d4efec3a6bfde6f034921dcde3cd6e0be08a45
Eric Martins <eric.martins@radixeng.com.br>
2021-03-17 11:35:07 -0300

    creates Category model and CateoriesRepository

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,36 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const newCategory = new Category();
+
+    Object.assign(newCategory, {
+      name,
+      description,
+      createdAt: new Date(),
+    });
+
+    this.categories.push(newCategory);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    return this.categories.find((category) => category.name === name);
+  }
+}
+
+export { CategoriesRepository };
