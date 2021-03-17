f0c77057818eacb64395587985151bcbcd2b2c5d
Eric Martins <eric.martins@radixeng.com.br>
2021-03-17 15:55:04 -0300

    Creates ISpecificationRepository and SpecificationRepository

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,27 @@
+import { Specification } from "../model/Specification";
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const newSpecification = new Specification();
+
+    Object.assign(newSpecification, {
+      name,
+      description,
+      createdAt: new Date(),
+    });
+
+    this.specifications.push(newSpecification);
+  }
+}
+
+export { SpecificationsRepository };
